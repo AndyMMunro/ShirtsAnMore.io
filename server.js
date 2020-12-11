57fd98053a40e47440ce802a6e21ea311e951dec
AndyMMunro <agovitadventures@gmail.com>
2020-12-10 19:20:23 -0800

    added log in page and changed the file structure to component base with bootstrap as th FE vizuals

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-const express = require("express");
-const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
+var express = require("express");
+const { auth } = require('express-openid-connect');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// sets up Auth with auth0
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'LjuOGMEYn3KcvM9R14f5JzYPXIb8OLA4',
+  issuerBaseURL: 'https://dev-qi7-st8o.us.auth0.com'
+};
+
+// Requiring our models for syncing
+var db = require("./models");
 
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse application body as JSON
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+// Static directory
+app.use(express.static("public"));
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// Routes
+// =============================================================
+// require("./routes/html-routes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/post-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
