d8c6211516c0f5c7bc25aaf897b2bbb82cb250d5
AndyMMunro <agovitadventures@gmail.com>
2020-12-23 21:31:13 -0800

    product seed added, product routes, server end point for product, data base propication for all tables, completed

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-// const bookRoutes = require("./books");
-const productRoutes = require("./productsApi");
-// const { query } = require("express");
+const productRoutes = require("./products");
+
 
 // Book routes end points what how in the URL
-router.use("/productsApi", productRoutes);
-// router.use("/books", bookRoutes);
-// router.use("/bookApiQuery" + query, bookApiQuery)
+router.use("/products", productRoutes);
+
 module.exports = router;
 
